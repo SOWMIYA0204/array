a923fa6382ba512f02dcfd0846ed71094301d0ce
SOWMIYA0204 <135124221+SOWMIYA0204@users.noreply.github.com>
2023-11-16 22:07:00 +0530

    javascript array

---
+++ array.js
@@ -0,0 +1,22 @@
+// var q1=[100,200]
+// var q2=[100,200]
+// var q3=q1;
+// console.log(q1===q2)
+// console.log(q1===q3)
+// var q1=[100,200]
+//  var q2=[100,200]
+//  var q3=q1;
+// q1.push(40);
+// q2.push(80);
+// q3.push(180);
+// console.log(q1)
+// console.log(q2)
+// console.log(q3)
+
+var r1=[100,200];
+var r2=[400,500];
+var r3=[...r1,...r2];
+console.log(r1);
+console.log(r2);
+console.log(r3);
+
